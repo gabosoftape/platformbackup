433072f405a27f6897a1bf93cf53fceaacd51984
Gab <gabosoft>
2020-02-26 15:57:48 -0500

    remove fuec

--- turnoses.php
+++
@@ -1,4 +0,0 @@
-<?php 
-include '../config/conexion.php';
-$mysqli->query("INSERT INTO mensajes(UNIT, ZONE, POS_TIME, SPEED, LOCATION, CURR_TIME, UNIT_ID) VALUES('".$_GET['UNIT']."', '".$_GET['ZONE']."', '".$_GET['POS_TIME']."', '".$_GET['SPEED']."', '".$_GET['LOCATION']."', '".$_GET['CURR_TIME']."', '".$_GET['UNIT_ID']."')");			
-?>
