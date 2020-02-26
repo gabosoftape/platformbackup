8bb0dba6679ca0475a3f839f116b5bed3d13bffc
Gab <gabosoft>
2020-02-26 16:38:44 -0500

    update test fuec

--- config/conexion.php
+++ config/conexion.php
@@ -2,6 +2,6 @@
 $mysqli = new mysqli('localhost', 'root', 'DoomArt4544!', 'gpscontrol_ws');
 mysqli_set_charset($mysqli,'utf8');
 if ($mysqli->connect_errno) {
-	echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	echo "Fallo algo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 ?>
