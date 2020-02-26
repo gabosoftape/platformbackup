f6f95d6499e2b4c8533245d508fa02bec24ed173
Gab <gabosoft>
2020-02-26 18:02:13 -0500

    update meta db config

--- config/conexion.php
+++ config/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli('localhost', 'root', 'gabosoft1234!', 'gpscontrol_ws');
+$mysqli = new mysqli('localhost', 'gabosoft', 'gabosoft1234!', 'gpscontrol_ws');
 mysqli_set_charset($mysqli,'utf8');
 if ($mysqli->connect_errno) {
 	echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
