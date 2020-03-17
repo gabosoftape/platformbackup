52e27393e3f534b16c6cef9435cf113b8b8a74a3
Gab <gabosoft>
2020-03-17 14:13:29 -0500

    update module employee

--- bravo-fin-svg/models/maintenance.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class bravoFinMaintenance1(models.Model):
-    _name = 'bf.svg.tp_operation'
-    _description = 'Tipos de operación'
-
-    description = fields.Char('Descripción')
